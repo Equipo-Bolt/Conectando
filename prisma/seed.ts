3a2748796fbecc71cb1418be005b55bf31e8a034
Frank0125 <a01252974@tec.mx>
2025-04-11 03:28:56 -0700

    ADD: Seed file finished, creates 3 users, where two have forms

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+import { connect } from "http2";
 import { prisma } from "../src/lib/prisma";
 import * as fs from "fs";
 
@@ -5,10 +6,7 @@ import * as fs from "fs";
     ! In this file, catalogs and preliminary records will be declared to populate the db
     
     * First, we will be include the catalogs
-    * Next, entities with relations:
-    *   - Users with form in draft
-    *   - Users with form with objectives
-    *   - Users with Collaborators
+    * Next, entities using upsert function.
 */
 async function main() {
   //! ------------------------- Catalogs --------------------------------------
@@ -26,69 +24,262 @@ async function main() {
   const catalogProgresses: string[] = JSON.parse(rawProgresses);
   const catalogRoles: string[] = JSON.parse(rawRoles);
 
-  await Promise.all(
-    catalogAreas.map((title) =>
-      prisma.area.upsert({
+  for (const title of catalogAreas) { //! using for to insert in order
+    await prisma.area.upsert({
+      where: { title },
+      create: { title },
+      update: { title },
+    });
+  }
+
+  for (const title of catalogDivisions) {
+    await prisma.division.upsert({
+      where: { title },
+      create: { title },
+      update: { title },
+    });
+  }
+
+  for (const bu of catalogBusinessUnits) {
+    await prisma.businessUnit.upsert({
+      where: { title: bu.title },
+      create: { title: bu.title, divisionID: bu.divisionID },
+      update: { title: bu.title },
+      });
+  }
+
+  for (const title of catalogClasifications) {
+      await prisma.classification.upsert({
         where: { title },
         create: { title },
         update: { title },
-      })
-    )
-  );
+      });
+  }
 
-  await Promise.all(
-    catalogDivisions.map((title) =>
-      prisma.division.upsert({
+  for (const title of catalogProgresses) {
+      await prisma.progress.upsert({
         where: { title },
         create: { title },
         update: { title },
-      })
-    )
-  );
+      });
+  }
 
-  await Promise.all(
-    catalogBusinessUnits.map((bu) =>
-      prisma.businessUnit.upsert({
-        where: { title: bu.title },
-        create: { title: bu.title, divisionID: bu.divisionID },
-        update: { title: bu.title },
-      })
-    )
-  );
-
-  await Promise.all(
-    catalogClasifications.map((title) =>
-      prisma.classification.upsert({
+  for (const title of catalogRoles) {
+      await prisma.role.upsert({
         where: { title },
         create: { title },
         update: { title },
-      })
-    )
-  );
-
-  await Promise.all(
-    catalogProgresses.map((title) =>
-      prisma.progress.upsert({
-        where: { title },
-        create: { title },
-        update: { title },
-      })
-    )
-  );
-
-  await Promise.all(
-    catalogRoles.map((title) =>
-      prisma.role.upsert({
-        where: { title },
-        create: { title },
-        update: { title },
-      })
-    )
-  );
+      });
+  }
 
   //! ------------------------- Catalogs --------------------------------------
 
-  console.log("Catalogs and Preliminar data seeded");
+  //! ------------------------- Dummy Data -------------------------------}
+  
+  //* Users
+  const userBoss = await prisma.user.upsert({
+    where: { employeeNumber: 22222 }, //* We can use the employee number field to search the user
+    update: {
+      fullName: "Daniel Fernández",
+      email: "daniel@gmail.com",
+      jobPosition: "Manager de Software",
+      positionSeniority: 12,
+      companySeniority: 36,
+      companyContribution: "Mi puesto como manager de software " +
+        "contribuye a la empresa en lograr estar siempre " +
+        "actualizados en ámbitos tecnológicos.",
+      role: {
+        connect: { //* connect allows to relate to an entry with unique field
+          id: 4 //* connect Daniel to the role with id 4
+        }
+      },
+      businessUnit: {
+        connect: {
+          id: 6
+        }
+      },
+      area: {
+        connect: {
+          id: 10
+        }
+      },
+    },
+    create: {
+      employeeNumber: 22222,
+      fullName: "Daniel Fernández",
+      email: "daniel@gmail.com",
+      jobPosition: "Manager de Software",
+      positionSeniority: 12,
+      companySeniority: 36,
+      companyContribution: "Mi puesto como manager de software " +
+        "contribuye a la empresa en lograr estar siempre " +
+        "actualizados en ámbitos tecnológicos.",
+      boss: {
+        create: { //* Boss of Daniel
+          employeeNumber: 11111, //! this shouldn't be done in actual logic
+          fullName: "Juan Gutierrez",
+          jobPosition: "Jefe del sector agrícola",
+          roleID: 4
+        }
+      },
+      role: {
+        connect: {
+          id: 4
+        }
+      },
+      businessUnit: {
+        connect: {
+          id: 6
+        }
+      },
+      area: {
+        connect: {
+          id: 10
+        }
+      },
+    },
+  });
+  
+  const userCollaborator = await prisma.user.upsert({
+    where: { employeeNumber: 33333 },
+    update: {
+      fullName: "Andrés Sandoval Ibarra",
+      email: "andres@gmail.com",
+      jobPosition: "Coordinado de Puesto TI",
+      positionSeniority: 1,
+      companySeniority: 1,
+      companyContribution: "",
+      boss: { //* Boss of Andres (daniel)
+        connect: {
+          id: (await userBoss).id //* since boss was created above we can access its id
+        }
+      },
+      role: {
+        connect: {
+          id: 1
+        }
+      },
+      businessUnit: {
+        connect: {
+          id: 1
+        }
+      },
+      area: {
+        connect: {
+          id: 3
+        }
+      },
+    },
+    create: {
+      employeeNumber: 33333,
+      fullName: "Andrés Sandoval Ibarra",
+      email: "andres@gmail.com",
+      jobPosition: "Coordinador de Puesto TI",
+      positionSeniority: 1,
+      companySeniority: 1,
+      companyContribution: "",
+      boss: {
+        connect: {
+          id: (await userBoss).id
+        }
+      },
+      role: {
+        connect: {
+          id: 1
+        }
+      },
+      businessUnit: {
+        connect: {
+          id: 1
+        }
+      },
+      area: {
+        connect: {
+          id: 3
+        }
+      },
+    },
+  });
+  
+  //*Forms
+  const formCollaborator = await prisma.form.upsert({
+    where: { id : 1 },
+    update: {
+      collaborator: {
+        connect: {
+          id: (await userCollaborator).id
+        }
+      },
+      evaluator: {
+        connect: {
+          id: (await userBoss).id
+        }
+      },
+      progress: {
+        connect: {
+          id: 1
+        }
+      }
+    },
+    create: {
+      collaborator: {
+        connect: {
+          id: (await userCollaborator).id
+        }
+      },
+      evaluator: {
+        connect: {
+          id: (await userBoss).id
+        }
+      },
+      progress: {
+        connect: {
+          id: 1
+        }
+      }
+    }
+  })
+
+  const formBoss = await prisma.form.upsert({
+    where: { id : 2 },
+    update: {
+      collaborator: {
+        connect: {
+          id: (await userBoss).id
+        }
+      },
+      evaluator: {
+        connect: {
+          employeeNumber: 11111
+        }
+      },
+      progress: {
+        connect: {
+          id: 1
+        }
+      }
+    },
+    create: {
+      collaborator: {
+        connect: {
+          id: (await userBoss).id
+        }
+      },
+      evaluator: {
+        connect: {
+          employeeNumber: 11111
+        }
+      },
+      progress: {
+        connect: {
+          id: 1
+        }
+      }
+    }
+  })
+  
+  //! ------------------------- Dummy Data -------------------------------
+  console.log("Catalogs and Dummy data seeded.");
 }
 
 main()
