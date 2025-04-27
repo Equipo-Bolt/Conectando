b47a8c31be109605799cd6ad37e6c4b55b67bc65
Santiago Eduardo Poblete Talamante <a01254609@tec.mx>
2025-04-27 16:57:15 -0700

    MODIFY: cancel and submit button

--- src/components/CancelButton.tsx
+++ src/components/CancelButton.tsx
@@ -10,7 +10,7 @@ export interface ButtonProps {
 export default function CancelButton ( { route, text }: ButtonProps) {
 
     return (
-        <Button type="submit" className={`bg-white text-gemso-blue border-3 border-gemso-blue w-[10rem] h-[3rem] rounded-lg font-bold text-lg hover:bg-gemso-blue/3`} asChild>
+        <Button type="submit" className="bg-white text-gemso-blue border-3 border-gemso-blue w-[10rem] h-[3rem] rounded-lg font-bold text-lg hover:bg-gemso-blue/3" asChild>
             <Link href={route}>
                 {text}
             </Link>
--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -9,6 +9,6 @@ export interface ButtonProps {
 export default function SubmitButton( { onSubmit, text }: ButtonProps) {
 
     return (
-        <Button type="submit" className={`bg-gemso-blue w-[10rem] h-[3rem] rounded-lg font-bold text-lg hover:bg-gemso-blue/90`} onClick={onSubmit}>{text}</Button>
+        <Button type="submit" className="bg-gemso-blue w-[10rem] h-[3rem] rounded-lg font-bold text-lg hover:bg-gemso-blue/90" onClick={onSubmit}>{text}</Button>
     );
 }
