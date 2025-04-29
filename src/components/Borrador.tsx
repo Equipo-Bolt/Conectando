3852929888233b9d9b44448e6886d94af70eab8d
MeAndrew24 <a01253138@tec.mx>
2025-04-29 11:16:26 -0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Borrador.tsx 	new file:   src/components/InfoHover.tsx 	new file:   src/components/ui/hover-card.tsx 	new file:   src/components/ui/tooltip.tsx

--- src/components/Borrador.tsx
+++ src/components/Borrador.tsx
@@ -1,3 +1,11 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import InfoHover from "./infoHover";
+
 export default function Borrador() {
   return (
     <div>
@@ -5,24 +13,25 @@ export default function Borrador() {
         Estado: Borrador
       </div>
 
-      <div className="text-lg">
-        Para habilitar el envió de tu objetivos a revisión es necesario:
-      </div>
-
-      <div className="text-lg ml-2 space-y-1">
-        <div>1. Tener tu información de usuario completa.</div>
-        <div>
-          2. Que la suma de los pesos de las clasificaciones de objetivos sea
-          igual a 100.
+      <InfoHover>
+        <div className="text-sm mb-2">
+          Para habilitar el envío de tus objetivos a revisión es necesario:
         </div>
-        <div>
-          3. Tener al menos un objetivo por cada clasificación de objetivo.
+        <div className="text-sm ml-2 space-y-1">
+          <div>1. Tener tu información de usuario completa.</div>
+          <div>
+            2. Que la suma de los pesos de las clasificaciones de objetivos sea
+            igual a 100.
+          </div>
+          <div>
+            3. Tener al menos un objetivo por cada clasificación de objetivo.
+          </div>
+          <div>
+            4. Que la suma de los pesos de objetivos por cada clasificación sea
+            igual a 100.
+          </div>
         </div>
-        <div>
-          4. Que la suma de los pesos de objetivos por cada clasificación sea
-          igual a 100.
-        </div>
-      </div>
+      </InfoHover>
     </div>
   );
 }
