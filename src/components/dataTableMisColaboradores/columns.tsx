9616dbed35ab36593fa70f4d59bdb514cf612987
Santiago Eduardo Poblete Talamante <a01254609@tec.mx>
2025-05-13 02:41:01 -0700

    ADD: my collaborators page

---
+++ src/components/dataTableMisColaboradores/columns.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { TypeMyColaborator } from "@/types/TypeMyColaborator";
+
+import {ArrowRightEndOnRectangleIcon} from "@heroicons/react/24/outline";
+
+export const columns: ColumnDef<TypeMyColaborator>[] = [
+  {
+    accessorKey: "user",
+    header: "Colaborador",
+    size: 100,
+    cell: ({ row }) => {
+      const userName = row.original.user.fullName;
+      return <span>{userName}</span>;
+    },
+  },
+  {
+    accessorKey: "progress",
+    header: "Estado",
+    size: 100,
+    cell: ({ row }) => {
+      const formState = row.original.progress.title;
+      return (
+        <div className="flex items-center">
+          <span className="ml-2">{formState}</span>
+        </div>
+      );
+    }
+  },
+  {
+    accessorKey: "form",
+    header: "Ir a Propuesta",
+    size: 100,
+    cell: ({ row }) => {
+      const userId = row.original.user.id;
+      return (
+        <div className="flex items-center">
+          <a href={`/misColaboradores/${userId}`}>
+            <ArrowRightEndOnRectangleIcon className="w-5 h-5 text-gemso-blue" />
+          </a>
+        </div>
+      );
+    },
+  }
+];
