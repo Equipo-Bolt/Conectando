ceea23999263dff5325b6d15ba670a1d87c87c60
danielagallardoc <a01252869@tec.mx>
2025-05-19 20:37:13 -0700

    tabla corregida

--- src/app/(routes)/(tablero)/misColaboradores/page.tsx
+++ src/app/(routes)/(tablero)/misColaboradores/page.tsx
@@ -6,19 +6,18 @@ import { DataTableMyCollaborators } from "@/components/bolt/DataTables/dataTable
 import { columns } from "@/components/bolt/DataTables/dataTableMyCollaborator/columns";
 
 import { cookies } from "next/headers";
-
 import { getFormById } from "@/lib/fetches/form/getFormById";
 
 /**
  * @description
- * This is the page is where a user with the boss role can view the state of their collaborators' objectives.
- *
+ * This is the page where a user with the boss role can view the state of their collaborators' objectives.
  */
 async function MyCollaboratorsPage() {
-  //* Using cookies
+  // Get user ID from cookies
   const cookieStore = await cookies();
   const userId = cookieStore.get("userId")?.value;
 
+  // Get collaborators and their progress
   const collaborators = await getAllCollaboratorsOfBoss(Number(userId));
   const progresses = await Promise.all(
     collaborators.map(async (collaborator) => {
@@ -26,7 +25,7 @@ async function MyCollaboratorsPage() {
       if (formId === "Sin Formulario Activo") {
         return {
           user: collaborator,
-          progress: { id: 5, title: "Sin Formulario", createdAt: "today" }, //!neeeds change
+          progress: { id: 5, title: "Sin Formulario", createdAt: "today" }, // puedes ajustar esto si quieres
         };
       }
       const form = await getFormById(parseInt(formId));
@@ -39,15 +38,9 @@ async function MyCollaboratorsPage() {
   );
 
   return (
-    <div>
-      <h1 className="text-3xl  font-bold mb-[1rem]">Mis Colaboradores</h1>
-      <div className="container mx-auto">
-        {progresses.map((item, index) => (
-          <div key={index}>
-            <DataTableMyCollaborators columns={columns} data={[item]} />
-          </div>
-        ))}
-      </div>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Mis Colaboradores</h1>
+      <DataTableMyCollaborators columns={columns} data={progresses} />
     </div>
   );
 }
