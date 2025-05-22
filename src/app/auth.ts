9141dcac40f2597fc6507208d1e14245e262bb81
Frank0125 <a01252974@tec.mx>
2025-05-22 00:14:28 -0700

    FIX: changed auth configuration and added style to husky script

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,8 +1,15 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/lib/prisma"
+import authConfig from "./auth.config"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth
+    },
+  },
+  ...authConfig
 })
