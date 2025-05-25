4b99a0bc6fb3d19cd91d6cd6cfddb0a1df750974
gomezramon-dev <gomez.ramon2004@gmail.com>
2025-05-25 13:55:56 -0700

    ADD: middleware that protects routes

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,13 +1,36 @@
+import { NextResponse } from "next/server";
 import { auth } from "@/app/auth";
 
-// TODO find middleware that protects routes
-export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== "/login") {
-    const newUrl = new URL("/login", req.nextUrl.origin)
-    return Response.redirect(newUrl)
+const PUBLIC_ROUTES = ["/login", "/otp"];
+const PROTECTED_DEFAULT_ROUTE = "/misObjetivos";
+const LOGIN_ROUTE = "/login";
+
+export default auth(async (req) => {
+  const { nextUrl } = req;
+  const isAuthenticated = !!req.auth;
+
+  const isPublicRoute = PUBLIC_ROUTES.some(path => nextUrl.pathname.startsWith(path));
+  const isProtectedRoute = config.matcher.some(path => nextUrl.pathname.startsWith(path));
+
+  if (isPublicRoute) {
+    if (isAuthenticated && (nextUrl.pathname.startsWith("/login") || nextUrl.pathname.startsWith("/otp"))) {
+      return NextResponse.redirect(new URL(PROTECTED_DEFAULT_ROUTE, nextUrl.origin))
+    }
+    return NextResponse.next();
   }
+
+
+  if (isProtectedRoute) {
+    if (!isAuthenticated) {
+      const callbackUrl = nextUrl.pathname + nextUrl.search;
+      return NextResponse.redirect(new URL(`${LOGIN_ROUTE}?callbackUrl=${encodeURIComponent(callbackUrl)}`, nextUrl.origin));
+    }
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
 });
 
 export const config = {
-  matcher: ["/misObjetivos", "/misColaboradores"],
+  matcher: ["/misObjetivos/:path*", "/misColaboradores/:path*"],
 };
