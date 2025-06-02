7740b58b302f77d86ea728f4673db9fd61cb1291
gomezramon-dev <gomez.ramon2004@gmail.com>
2025-06-01 22:38:32 -0700

    FIX: middleware now runs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from 'next/server';
-import { auth } from "@/app/auth";
-import { Session } from "next-auth";
+
+import { auth } from "./app/auth";
 
 const PUBLIC_ROUTES = ["/login", "/otp"];
 const PROTECTED_DEFAULT_ROUTE = "/misObjetivos";
@@ -20,6 +20,8 @@ const ROLES = {
 const ROLE_PROTECTED_ROUTES_CONFIG: Record<string, number[]> = {
   "/misObjetivos": [
     ROLES.Colaborador, 
+    ROLES["Jefe Directo"],
+    ROLES.Administrador,
     ROLES["Colaborador y Jefe Directo"], 
     ROLES["Colaborador y Administrador"], 
     ROLES["Colaborador, Jefe Directo y Administrador"]
@@ -32,13 +34,11 @@ const ROLE_PROTECTED_ROUTES_CONFIG: Record<string, number[]> = {
   ]
 }
 
-interface NextAuthRequest extends NextRequest {
-  auth: Session | null;
-}
-
-export default auth(async (req: NextAuthRequest) => {
+export async function middleware(req: NextRequest) {
   const { nextUrl } = req;
-  const session = req.auth;
+
+  const session = await auth();
+  console.log(session)
   const isAuthenticated = !!session;
   const userRole = session?.user?.roleID;
   const currentPath = nextUrl.pathname;
@@ -48,14 +48,17 @@ export default auth(async (req: NextAuthRequest) => {
   // If user is already authenticated and attempts to access the login or otp page
   if (isPublicRoute) {
     if (isAuthenticated) {
+      console.log("IS AUTH AND PUBLIC")
       return NextResponse.redirect(new URL(PROTECTED_DEFAULT_ROUTE, nextUrl.origin))
     }
+    console.log("IS PUBLIC")
     return NextResponse.next();
   }
 
 
    // If user is not authenticated
   if (!isAuthenticated) {
+    console.log("IS NOT AUTH")
     const callbackUrl = currentPath + nextUrl.search;
     return NextResponse.redirect(new URL(`${LOGIN_ROUTE}?callbackUrl=${encodeURIComponent(callbackUrl)}`, nextUrl.origin));
   }
@@ -68,14 +71,18 @@ export default auth(async (req: NextAuthRequest) => {
   if (protectedRouteConfigKey) {
     const allowedRolesForPath = ROLE_PROTECTED_ROUTES_CONFIG[protectedRouteConfigKey];
     if (!userRole || !allowedRolesForPath.includes(userRole)) {
+      console.log("IS PROTECTED AND AUTH")
       const callbackUrl = currentPath + nextUrl.search;
       return NextResponse.redirect(new URL(`${LOGIN_ROUTE}?callbackUrl=${encodeURIComponent(callbackUrl)}`, nextUrl.origin));
     }
   }
 
   return NextResponse.next();
-});
+};
 
 export const config = {
-  matcher: ["/misObjetivos/:path*", "/misColaboradores/:path*"],
+  matcher: [
+    "/misObjetivos/:path*", 
+    "/misColaboradores/:path*"
+  ],
 };
