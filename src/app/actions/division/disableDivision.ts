7ca45fb11faea6d3b07d1b8769104673748bd69b
Frank0125 <a01252974@tec.mx>
2025-06-02 23:14:21 -0700

    ADD: implemented disable actions for all catalogs, disabling a division disables all children business units

--- src/app/actions/division/disableDivision.ts
+++ src/app/actions/division/disableDivision.ts
@@ -1,20 +1,42 @@
 "use server";
 
-import { prisma } from '@/lib/prisma';
+import { prisma } from "@/lib/prisma";
 
-export async function disableDivisionAction(divisionId : number) {
-    if (!divisionId){
-        throw new Error ("No id given to disable classification");
-    }
+import { ServerActionResponse } from "@/types/ServerActionResponse";
 
-    try {
-        await prisma.classification.update({
-            where: { id : divisionId },
-            data: { deactivated : true }
-        })
+/**
+ * * disableDivisionAction() This action disables one Division
+ * @param prevState<string | null> Initial state of action, set this parameter to null
+ * @param divisionId<number> ID of Division to disable
+ * @returns Promise of type {@link ServerActionResponse}
+ */
+export async function disableDivisionAction(
+  prevState: ServerActionResponse | null,
+  divisionId: number
+): Promise<ServerActionResponse> {
+  try {
+    // ! prisma.$transaction tries to execute both querries but if one fails, none are done 
+    await prisma.$transaction([
+      // * Disable division
+      prisma.division.update({
+        where: { id: divisionId },
+        data: { deactivated: true },
+      }),
 
-        return "Classification has been disabled";
-    } catch {
-        throw new Error ("Failed to disable classification")
-    }
+      // * Disable business units realted to division
+      prisma.businessUnit.updateMany({
+        where: {
+          divisionID: divisionId,
+        },
+        data: {
+          deactivated: true,
+        },
+      }),
+    ]);
+
+    return { success: true, message: "Division ha sido deshabilitada" };
+  } catch (error) {
+    console.log(`Error when disabling Division: ${(error as Error).message}`);
+    return { success: false, error: `${(error as Error).message}` };
+  }
 }
