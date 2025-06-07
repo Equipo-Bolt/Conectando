4611ec2db6b99dbe2931502b7068e415720d18fc
Santiago Eduardo Poblete Talamante <a01254609@tec.mx>
2025-06-06 19:31:52 -0700

    FIX: small fix for pagination, still doesn't look very good but looks better

--- src/components/bolt/Paginations/Pagination.tsx
+++ src/components/bolt/Paginations/Pagination.tsx
@@ -7,6 +7,7 @@ import {
   PaginationLink,
   PaginationPrevious,
   PaginationNext,
+  PaginationEllipsis,
 } from "@/components/ui/pagination";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
@@ -26,6 +27,70 @@ export function PaginationComponent({ totalPages, currentPage }: PaginationProps
     router.push(`${pathname}?${params.toString()}`);
   };
 
+  const renderPageNumbers = () => {
+    const pages = [];
+    
+    // Always show first page
+    pages.push(
+      <PaginationItem key={1}>
+        <PaginationLink
+          isActive={1 === currentPage}
+          onClick={() => handlePageChange(1)}
+        >
+          1
+        </PaginationLink>
+      </PaginationItem>
+    );
+
+    // Show ellipsis if current page is greater than 2
+    if (currentPage > 2) {
+      pages.push(
+        <PaginationItem key="ellipsis-start">
+          <PaginationEllipsis />
+        </PaginationItem>
+      );
+    }
+
+    // Show current page if it's not 1 or the last page
+    if (currentPage > 1 && currentPage < totalPages) {
+      pages.push(
+        <PaginationItem key={currentPage}>
+          <PaginationLink
+            isActive={true}
+            onClick={() => handlePageChange(currentPage)}
+          >
+            {currentPage}
+          </PaginationLink>
+        </PaginationItem>
+      );
+    }
+
+    // Show ellipsis before last page if there's a gap
+    if (currentPage < totalPages - 1 && totalPages > 2) {
+      pages.push(
+        <PaginationItem key="ellipsis-end">
+          <PaginationEllipsis />
+        </PaginationItem>
+      );
+    }
+
+    // Always show last page if there are more than 1 page
+    if (totalPages > 1) {
+      pages.push(
+        <PaginationItem key={totalPages}>
+          <PaginationLink
+            isActive={totalPages === currentPage}
+            onClick={() => handlePageChange(totalPages)}
+          >
+            {totalPages}
+          </PaginationLink>
+        </PaginationItem>
+      );
+    }
+
+    return pages;
+  };
+
   return (
     <Pagination>
       <PaginationContent>
@@ -36,16 +101,7 @@ export function PaginationComponent({ totalPages, currentPage }: PaginationProps
           />
         </PaginationItem>
 
-        {Array.from({ length: totalPages }, (_, i) => (
-          <PaginationItem key={i}>
-            <PaginationLink
-              isActive={i + 1 === currentPage}
-              onClick={() => handlePageChange(i + 1)}
-            >
-              {i + 1}
-            </PaginationLink>
-          </PaginationItem>
-        ))}
+        {renderPageNumbers()}
 
         <PaginationItem>
           <PaginationNext
